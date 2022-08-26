5dc52b1a5c970b11eeee44b9434a9116a09f6b74
vrishabh081 <rishabhvishwakarma398@gmail.com>
2022-08-26 15:27:51 -0700

    added day-2work

---
+++ scripts/part2.js
@@ -0,0 +1,26 @@
+let part2 = ()=>
+{
+    return`
+    <div>
+        <div class="img">
+            <img src="https://espn.wonderwins.com/assets/images/main_logo.png" alt="">
+        </div>
+        <h1>
+            INDIA'S COOLEST
+            <span>
+                FANTASY PLATFORM
+            </span>
+        </h1>
+        <h2>
+            Download the App
+        </h2>
+        <a href="">
+            <img src="https://espn.wonderwins.com/assets/images/android_icon.png" alt="">
+        </a>
+        <a href="">
+            <img src="https://espn.wonderwins.com/assets/images/ios_icon.png" alt="">
+        </a>
+    </div>`
+}
+
+export default part2()
